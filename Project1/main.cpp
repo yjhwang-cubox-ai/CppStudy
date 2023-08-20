fb60ba10cd726846afc84de3d81d71967b66c56d
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2023-08-21 08:41:08 +0900

    default copy constructor

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,39 +1,21 @@
 #include <iostream>
-#include <string>
 
 class Cat {
 public:
 	Cat() = default;
-	Cat(std::string name, int age) : mName{ std::move(name) }, mAge(age)
-	{
-		std::cout << mName << " constructor" << std::endl;
-	};
-	Cat(const Cat& other) : mName{ std::move(other.mName) }, mAge{ other.mAge }{
-		std::cout << mName << " copy constructor" << std::endl;
+	Cat(int age) : mAge(age) { std::cout << "constructor!" << std::endl; }
+	void speak() {
+		std::cout << mAge << std::endl;
 	}
-	Cat(Cat&& other) :mName{ std::move(other.mName) }, mAge{ other.mAge }{
-		std::cout << mName << " move constructor" << std::endl;
-	}
-	~Cat()
-	{
-		std::cout << mName << "destructor" << std::endl;
-	}
-	void speakName() {
-		std::cout << mName << std::endl;
-	}
-
 private:
 	int mAge;
-	std::string mName;
 };
 
 int main() {
-	Cat kitty{"kitty", 1};
-	Cat nabi = kitty;
-	Cat nabi2{ std::move(kitty) };
-	kitty.speakName();
-	nabi.speakName();
-	nabi2.speakName();
+	Cat cat(33);
+	Cat cat2(cat);
+	cat.speak();
+	cat2.speak();
 
 	return 0;
 }
